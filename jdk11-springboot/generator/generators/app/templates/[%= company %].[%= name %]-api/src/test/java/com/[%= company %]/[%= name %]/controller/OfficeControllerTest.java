a82f4e7f7430c3bf1258b9b5686c7ffd4e66fc1e
Guillermo Segura <guillermo.segura@axity.com>
2022-07-26 20:09:52 -0500

    Ajuste de generador del arquetipo

--- jdk11-springboot/generator/generators/app/templates/[%= company %].[%= name %]-api/src/test/java/com/[%= company %]/[%= name %]/controller/OfficeControllerTest.java
+++ jdk11-springboot/generator/generators/app/templates/[%= company %].[%= name %]-api/src/test/java/com/[%= company %]/[%= name %]/controller/OfficeControllerTest.java
@@ -75,7 +75,16 @@ class OfficeControllerTest
   @Test
   void testFindOffice()
   {
-    fail( "Not yet implemented" );
+    var office = this.createOffice( 1 );
+    var generic = new GenericResponseDto<>(office);
+    when( this.officeFacade.find( anyString() ) ).thenReturn( generic );
+    
+    MvcResult result = mockMvc.perform( MockMvcRequestBuilders.get( "/api/offices/1" ) )
+        .andExpect( status().isOk() )
+        .andExpect( jsonPath( "$.header.code" ).value( "0" ) )
+        .andExpect( jsonPath( "$.body.officeId" ).value( "1" ) ).andReturn();
+
+    assertNotNull( result );
   }
 
   /**
@@ -85,7 +94,21 @@ class OfficeControllerTest
   @Test
   void testCreate()
   {
-    fail( "Not yet implemented" );
+    var office = this.createOffice( 9 );
+    var generic = new GenericResponseDto<>(office);
+    when(this.officeFacade.create( any( OfficeDto.class ) )).thenReturn( generic );
+    
+    Gson gson = new GsonBuilder().create();
+    
+    MvcResult result = mockMvc.perform( MockMvcRequestBuilders.post( "/api/offices"  )
+            .content(gson.toJson(office))
+            .accept( MediaType.APPLICATION_JSON )
+            .contentType( MediaType.APPLICATION_JSON ))
+        .andExpect( status().isCreated() )
+        .andExpect( jsonPath( "$.header.code" ).value( "0" ) )
+        .andExpect( jsonPath( "$.body.officeId" ).value( "9" ) ).andReturn();
+
+    assertNotNull( result );
   }
 
   /**
@@ -95,7 +118,21 @@ class OfficeControllerTest
   @Test
   void testUpdate()
   {
-    fail( "Not yet implemented" );
+    var office = this.createOffice( 1 );
+    var generic = new GenericResponseDto<>(true);
+    when(this.officeFacade.update( any( OfficeDto.class ) )).thenReturn( generic );
+    
+    Gson gson = new GsonBuilder().create();
+    
+    MvcResult result = mockMvc.perform( MockMvcRequestBuilders.put( "/api/offices/1"  )
+            .content(gson.toJson(office))
+            .accept( MediaType.APPLICATION_JSON )
+            .contentType( MediaType.APPLICATION_JSON ))
+        .andExpect( status().isOk() )
+        .andExpect( jsonPath( "$.header.code" ).value( "0" ) )
+        .andExpect( jsonPath( "$.body" ).value( "true" ) ).andReturn();
+
+    assertNotNull( result );
   }
 
   /**
@@ -104,7 +141,16 @@ class OfficeControllerTest
   @Test
   void testDelete()
   {
-    fail( "Not yet implemented" );
+    var generic = new GenericResponseDto<>(true);
+    when(this.officeFacade.delete( anyString() )).thenReturn( generic );
+    
+    MvcResult result = mockMvc.perform( MockMvcRequestBuilders.delete( "/api/offices/1"  )
+            .accept( MediaType.APPLICATION_JSON ))
+        .andExpect( status().isOk() )
+        .andExpect( jsonPath( "$.header.code" ).value( "0" ) )
+        .andExpect( jsonPath( "$.body" ).value( "true" ) ).andReturn();
+
+    assertNotNull( result );
   }
 
   private OfficeDto createOffice( int i )
